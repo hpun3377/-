696aba8a1f81a015ea016488088b600d56742539
sungwon.park <hpun3377@gmail.com>
2016-04-22 11:29:55 +0900

    여론조사 사이트 만들기 여론조사 결과 저장까지 만듦.

--- tutorial/tutorial/tutorial/settings.py
+++ tutorial/tutorial/tutorial/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_mongoengine',
     'rest_framework',
-    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-   'default' : {
-      'ENGINE' : 'django_mongodb_engine',
-      'NAME' : 'my_database'
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
